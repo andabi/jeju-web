eb9e39a5765d949b978e58fadfff7eaba2b8fe41
jun0cho <jun0cho@gmail.com>
2015-03-26 15:59:35 +0000

    [NOT WORK] alert and move page

--- app/scripts/controllers/write.js
+++ app/scripts/controllers/write.js
@@ -1,12 +1,14 @@
 'use strict';
 
 angular.module('webApp')
-  .controller('WriteCtrl', function ($rootScope, $scope, $log, Messages) {
+  .controller('WriteCtrl', function ($rootScope, $scope, $window, $log, Messages) {
     $rootScope.nav = 'write';
 
     $scope.send = function () {
+      alert("Complete!");
       Messages.sendMsg($scope.writer, $scope.content);
       $log.debug("Writer: " + $scope.writer);
       $log.debug("Content: " + $scope.content);
+      $window.location.href = '/show';
     }
   });
