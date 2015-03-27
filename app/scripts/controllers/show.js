b723ae6874319b15d2775c01750f843fcf47aa44
안다비 <andabi@andabiui-MacBook-Air.local>
2015-03-27 21:47:21 +0900

    update

--- app/scripts/controllers/show.js
+++ app/scripts/controllers/show.js
@@ -3,16 +3,19 @@
 angular.module('webApp')
   .controller('ShowCtrl', function ($rootScope, $scope, $interval, $log, Messages) {
     $rootScope.nav = 'show';
+    $scope.noticeMsg = {content: '축하 메시지는 jeju.bigeye.me에서', writer: '소민♥️문범'};
 
     if($rootScope.isIntervalStarted === false) {
       $rootScope.data = [];
+      $scope.msgs = [];
       $rootScope.isIntervalStarted = true;
       $interval(function() {
         var newMsgPromise = Messages.getNewMsgPromise(Messages.lastTime);
         newMsgPromise.then(function(data) {
           if (data.length > 0) {
-            $rootScope.data = data.concat($rootScope.data);
             Messages.lastTime = data[0].created_at;
+            $scope.msgs = data.concat([$scope.noticeMsg]).concat($rootScope.data);
+            $rootScope.data = data.concat($rootScope.data);
           }
         }, function (data) {
           //error
